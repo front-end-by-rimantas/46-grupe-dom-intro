48f75584b06da93f5ac33f741a10faf72d563c59
Rimantas <belauzas@gmail.com>
2023-10-30 09:53:46 +0200

    DOm

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const hi = 'Labas rytas, Lietuva! 👋';
+console.log(hi);
+
+const titleDOM = document.getElementById('title');
+console.log(titleDOM);
+
+titleDOM.innerText = hi;
+
+console.log(titleDOM.classList);
+// titleDOM.classList.add('red');
+
+for (let i = 0; i < titleDOM.classList.length; i++) {
+    const c = titleDOM.classList[i];
+    titleDOM.classList.remove(c);
+}
+
+const sectionTitle = 'About you';
+// const sectionTitleDOM = document.querySelector('.section-title');
+// const sectionTitleDOM = document.querySelector('h2');
+const sectionTitleDOM = document.querySelector('h2.section-title > span');
+
+// sectionTitleDOM.innerText = sectionTitle;
+sectionTitleDOM.textContent = sectionTitle;
